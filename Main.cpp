80c9389485ad0452cb800d73cf1602071a09fef5
trufYm <142312069+trufYm@users.noreply.github.com>
2024-01-19 22:28:13 +0800

    Deleted comments

--- Main.cpp
+++ Main.cpp
@@ -12,20 +12,3 @@ int main() {
 
    return 0;
 }
-
-//Code for Square
-/*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Start-Up");
-   sf::CircleShape shape(100.f);
-   shape.setFillColor(sf::Color::Green);
-
-   while (window.isOpen()) {
-      sf::Event event;
-      while (window.pollEvent(event)) {
-         if (event.type == sf::Event::Closed)
-            window.close();
-      }
-
-      window.clear();
-      window.draw(shape);
-      window.display();
-   }*/
