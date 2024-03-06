d461d836791830305e7f39dfc8122185ab57b3db
trufYm <142312069+trufYm@users.noreply.github.com>
2024-03-06 19:14:47 +0800

    addendum to the addendum

--- Model/Scene/MainMenuScene.hpp
+++ Model/Scene/MainMenuScene.hpp
@@ -2,13 +2,16 @@
 
 #include "../Scene.hpp"
 #include "../Enum/SceneTag.hpp"
+#include "../Enum/AssetType.hpp"
 #include "../AnimatedTexture.hpp"
 #include "../Entity/Background.hpp"
 #include "../Entity/Player.hpp"
+#include "../../Controller/Manager/TextureManager.hpp"
 
 namespace scenes {
     using namespace models;
     using namespace entities;
+    using namespace managers;
     class MainMenuScene : public Scene {
         public:
             MainMenuScene();
